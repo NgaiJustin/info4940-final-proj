2eaa28a73ff3eefc9cad9d0348522381f56cb407
Justin Ngai <jn537@cornell.edu>
2022-05-03 17:12:32 -0400

    Setup template for racing bar graph

--- clearing-the-air/src/App.js
+++ clearing-the-air/src/App.js
@@ -1,46 +1,95 @@
 import React from 'react'
 import BarChart from './diagrams/BarChart'
+import BarChartRace from './diagrams/BarChartRace'
+
+import { useState } from 'react'
+import useInterval from './hooks/useInterval'
+
 import './App.css'
 
-const data = [
-  { year: 1980, efficiency: 24.3, sales: 8949000 },
-  { year: 1985, efficiency: 27.6, sales: 10979000 },
-  { year: 1990, efficiency: 28, sales: 9303000 },
-  { year: 1991, efficiency: 28.4, sales: 8185000 },
-  { year: 1992, efficiency: 27.9, sales: 8213000 },
-  { year: 1993, efficiency: 28.4, sales: 8518000 },
-  { year: 1994, efficiency: 28.3, sales: 8991000 },
-  { year: 1995, efficiency: 28.6, sales: 8620000 },
-  { year: 1996, efficiency: 28.5, sales: 8479000 },
-  { year: 1997, efficiency: 28.7, sales: 8217000 },
-  { year: 1998, efficiency: 28.8, sales: 8085000 },
-  { year: 1999, efficiency: 28.3, sales: 8638000 },
-  { year: 2000, efficiency: 28.5, sales: 8778000 },
-  { year: 2001, efficiency: 28.8, sales: 8352000 },
-  { year: 2002, efficiency: 29, sales: 8042000 },
-  { year: 2003, efficiency: 29.5, sales: 7556000 },
-  { year: 2004, efficiency: 29.5, sales: 7483000 },
-  { year: 2005, efficiency: 30.3, sales: 7660000 },
-  { year: 2006, efficiency: 30.1, sales: 7762000 },
-  { year: 2007, efficiency: 31.2, sales: 7562000 },
-  { year: 2008, efficiency: 31.5, sales: 6769000 },
-  { year: 2009, efficiency: 32.9, sales: 5402000 },
-  { year: 2010, efficiency: 33.9, sales: 5636000 },
-  { year: 2011, efficiency: 33.1, sales: 6093000 },
-  { year: 2012, efficiency: 35.3, sales: 7245000 },
-  { year: 2013, efficiency: 36.4, sales: 7586000 },
-  { year: 2014, efficiency: 36.5, sales: 7708000 },
-  { year: 2015, efficiency: 37.2, sales: 7517000 },
-  { year: 2016, efficiency: 37.7, sales: 6873000 },
-  { year: 2017, efficiency: 39.4, sales: 6081000 },
+const barData = [
+  { year: 1, efficiency: 24.3, sales: 100 },
+  { year: 2, efficiency: 27.6, sales: 61 },
+  { year: 3, efficiency: 28, sales: 59 },
+  { year: 4, efficiency: 28.4, sales: 65 },
 ]
 
+const getRandomIndex = array => {
+  return Math.floor(array.length * Math.random())
+}
+
 function App() {
+  const [iteration, setIteration] = useState(0)
+  const [start, setStart] = useState(false)
+  const [data, setData] = useState([
+    {
+      name: 'alpha',
+      value: 10,
+      color: '#f4efd3',
+    },
+    {
+      name: 'beta',
+      value: 15,
+      color: '#cccccc',
+    },
+    {
+      name: 'charlie',
+      value: 20,
+      color: '#c2b0c9',
+    },
+    {
+      name: 'delta',
+      value: 25,
+      color: '#9656a1',
+    },
+    {
+      name: 'echo',
+      value: 30,
+      color: '#fa697c',
+    },
+    {
+      name: 'foxtrot',
+      value: 35,
+      color: '#fcc169',
+    },
+  ])
+
+  useInterval(() => {
+    if (start) {
+      const randomIndex = getRandomIndex(data)
+      setData(
+        data.map((entry, index) =>
+          index === randomIndex
+            ? {
+                ...entry,
+                value: entry.value + 10,
+              }
+            : entry
+        )
+      )
+      setIteration(iteration + 1)
+    }
+  }, 500)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <BarChart data={data} />
-      </header>
+    <div className="root">
+      {/* Graph 1 */}
+      <div className="diagram">
+        <h1>Racing Bar Chart</h1>
+        <BarChartRace data={data} />
+        <button onClick={() => setStart(!start)}>
+          {start ? 'Start' : 'Stop!'}
+        </button>
+        <p>Iteration: {iteration}</p>
+      </div>
+      {/* Graph 2 */}
+      <div className="diagram">
+        <div className="barWrapper">
+          <BarChart data={barData} />
+        </div>
+      </div>
+      {/* Graph 3 */}
+      {/* Graph 4 */}
     </div>
   )
 }
