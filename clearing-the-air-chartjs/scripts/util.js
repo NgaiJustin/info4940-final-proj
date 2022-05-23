5f1ac9b27ae2e6db3621aa8d7f845ced7e502f7d
Justin Ngai <jn537@cornell.edu>
2022-05-22 23:56:48 -0400

    Remove old add stuff

--- clearing-the-air-chartjs/scripts/util.js
+++ clearing-the-air-chartjs/scripts/util.js
@@ -1,6 +1,7 @@
 const GOOGLE_COLOR_LIGHT = 'rgba(66, 133, 244, 0.2)'
 const GOOGLE_COLOR = 'rgba(66, 133, 244, 0.9)'
 const IBM_COLOR_LIGHT = 'rgba(0, 0, 0, 0.2)'
+const IBM_COLOR_MEDIUM = 'rgba(0, 0, 0, 0.6)'
 const IBM_COLOR = 'rgba(0, 0, 0, 0.9)'
 const ORACLE_COLOR_LIGHT = 'rgba(248, 0, 0, 0.2)'
 const ORACLE_COLOR = 'rgba(248, 0, 0, 0.9)'
